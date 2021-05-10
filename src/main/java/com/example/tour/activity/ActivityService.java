cf53b95d9864ffbc290aead00e1df434ad9a5f9c
Ivan Krasowski <ivankras@hotmail.com>
2021-05-10 12:22:43 -0500

    ADD basic Activity functionality and propose Activity

---
+++ src/main/java/com/example/tour/activity/ActivityService.java
@@ -0,0 +1,23 @@
+package com.example.tour.activity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ActivityService {
+    private final ActivityRepository activityRepository;
+
+    @Autowired
+    public ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    public void addNewActivity(Activity activity) {
+        activityRepository.save(activity);
+    }
+
+    public Activity getActivity(Long activityId) {
+        Activity activity = activityRepository.findById(activityId).orElseThrow(
+                () -> new IllegalStateException("Activity with id " + activityId + " does not exist")
+        );
+        return activity;
+    }
+}
