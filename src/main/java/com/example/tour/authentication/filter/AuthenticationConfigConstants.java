a77a88ed25e33a098c35c24540eeb8aded0d2353
Rebecca <rebeccayilma@gmail.com>
2021-05-11 12:14:00 -0500

    configs

--- src/main/java/com/example/tour/authentication/filter/AuthenticationConfigConstants.java
+++ src/main/java/com/example/tour/authentication/filter/AuthenticationConfigConstants.java
@@ -5,5 +5,5 @@ public class AuthenticationConfigConstants {
     public static final long EXPIRATION_TIME = 3600000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/sign_up";
+    public static final String SIGN_UP_URL = "/user/new";
 }
