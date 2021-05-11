21de22de8a0420d14886b15b26e96a5ac597e271
Rebecca <rebeccayilma@gmail.com>
2021-05-11 10:47:03 -0500

    User Table

---
+++ src/main/java/com/example/tour/authentication/model/User.java
@@ -0,0 +1,23 @@
+package com.example.tour.authentication.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+}
