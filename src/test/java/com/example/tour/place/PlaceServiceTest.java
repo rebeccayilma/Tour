57696a41db0a57e1cc9ee12b514bd6ba71a1374d
Raey Fikre <raeycareer@gmail.com>
2021-05-13 23:00:40 -0500

    PlaceServiceTest-mockTest

---
+++ src/test/java/com/example/tour/place/PlaceServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.tour.place;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PlaceServiceTest {
+    @Mock
+    private PlaceRepository placeRepository;
+    private PlaceService underTest;
+
+    @BeforeEach
+    void setUp() {
+      underTest = new PlaceService(placeRepository);
+    }
+    @Test
+    void canGetPlaces() {
+        underTest.getPlaces();
+        verify(placeRepository).findAll();
+    }
+    @Test
+    void canAddNewPlace() {
+        //given
+        Place place = new Place("NewPlace", 23.36, 52.36, "Welcome to Addis Ababa");
+        //when
+        underTest.addNewPlace(place);
+        //then
+        ArgumentCaptor<Place> placeArgumentCaptor = ArgumentCaptor.forClass(Place.class);
+        verify(placeRepository).save(placeArgumentCaptor.capture());
+
+        Place capturedPlace = placeArgumentCaptor.getValue();
+        assertThat(capturedPlace).isEqualTo(place);
+    }
+
+}
