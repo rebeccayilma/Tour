ae652078ae10090ef5c818d3340d9f42c9c65c88
Raey Fikre <78286318+raeyh@users.noreply.github.com>
2021-05-14 09:39:51 -0500

    Rating test (#23)
    * react-starting-pack
    
    * PlaceServiceTest-mockTest
    
    * PlaceServiceTest-mockTest
    
    * RatingTest-mockTest

--- src/main/java/com/example/tour/rating/RatingService.java
+++ src/main/java/com/example/tour/rating/RatingService.java
@@ -8,24 +8,26 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class RatingService  {
+public class RatingService {
     private RatingRepository ratingRepository;
     private ActivityRepository activityRepository;
 
 
     @Autowired
-    public RatingService(RatingRepository ratingRepository, ActivityRepository activityRepository){
+    public RatingService(RatingRepository ratingRepository, ActivityRepository activityRepository) {
         this.ratingRepository = ratingRepository;
         this.activityRepository = activityRepository;
 
 
     }
-    public List<Rating> getRatings(Long activityId){
+
+    public List<Rating> getRatings(Long activityId) {
         return ratingRepository.findByActivityId(activityId);
     }
+
     public void addNewRating(Rating rating) {
         Long activityId = rating.getActivity().getId();
-        Activity activity = activityRepository.findById(activityId).orElseThrow(()-> new IllegalStateException("No rating exist for "+ activityId+" activity"));
+        Activity activity = activityRepository.findById(activityId).orElseThrow(() -> new IllegalStateException("No rating exist for " + activityId + " activity"));
         rating.setActivity(activity);
         ratingRepository.save(rating);
     }
