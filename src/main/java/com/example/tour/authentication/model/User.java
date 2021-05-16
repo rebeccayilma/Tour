d7ef269fc67a78b419ffc299f50af009450068bc
rebeccayilma <rebeccayilma@gmail.com>
2021-05-16 18:08:06 -0500

    Authentication (#29)
    * Fix Login Issues
    
    * Remove Unessesary Class
    
    * send response in body
    
    * Remove role from user DTO
    
    * set default new user role to be "CONTRIBUTOR"
    
    * add dependency for validation
    
    * add validation for user role
    
    * add ADMIN when application starts
    
    Co-authored-by: Ivan Krasowski <ivankras@hotmail.com>

--- src/main/java/com/example/tour/authentication/model/User.java
+++ src/main/java/com/example/tour/authentication/model/User.java
@@ -1,11 +1,14 @@
 package com.example.tour.authentication.model;
 
-import com.example.tour.authentication.domain.Role;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -17,6 +20,6 @@ public class User {
     private Long id;
     private String username;
     private String password;
-    @Enumerated(EnumType.STRING)
-    private Role role;
+    @Pattern(regexp = "^(ADMIN|CONTRIBUTOR)$")
+    private String role;
 }
