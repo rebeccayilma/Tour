bfe41b6fab35aa1aa667eac7a81c7c0e8c75fdf3
ggrake <81604262+ggrake@users.noreply.github.com>
2021-05-16 10:42:53 -0500

    place, activity, user controller tests

--- src/main/java/com/example/tour/activity/Activity.java
+++ src/main/java/com/example/tour/activity/Activity.java
@@ -5,7 +5,9 @@ import com.example.tour.rating.Rating;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.ArrayList;
