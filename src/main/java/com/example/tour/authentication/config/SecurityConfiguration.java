b4508c7265408f71b84f364194e7eee376c96096
Rebecca <rebeccayilma@gmail.com>
2021-05-15 23:45:48 -0500

    Fix Login Issues

--- src/main/java/com/example/tour/authentication/config/SecurityConfiguration.java
+++ src/main/java/com/example/tour/authentication/config/SecurityConfiguration.java
@@ -22,10 +22,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http)throws Exception{
         http.cors().and().csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, AuthenticationConfigConstants.SIGN_UP_URL).permitAll()
-                .antMatchers("api/place").permitAll()
-                .antMatchers("/user/**","api/activity/approve/**","api/activity/inactive","api/activity/deactivate/**").hasAnyAuthority("ADMIN")
-                .antMatchers(HttpMethod.POST,"api/activity").hasAnyAuthority("CONTRIBUTOR")
-                .antMatchers(HttpMethod.POST,"api/place").hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/place").hasAnyAuthority("ADMIN")
+                .antMatchers("/api/place/**").permitAll()
+                .antMatchers(HttpMethod.PUT,"api/activity/approve/**","api/activity/deactivate/**").hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST,"/api/activity").hasAnyAuthority("CONTRIBUTOR")
                 .anyRequest().authenticated().and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
                 .addFilter(new JWTAuthorizationFilter(authenticationManager()))
