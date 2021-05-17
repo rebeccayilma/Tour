ac4e58793d45e902da2ddc8a1eabfd01aebd0012
Ivan Krasowski <ivankras@hotmail.com>
2021-05-17 07:52:12 -0500

    Fix inactiveActivities request (jwt missing)

--- src/main/viewTour/view/src/components/InactiveActivities.js
+++ src/main/viewTour/view/src/components/InactiveActivities.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { INACTIVE_ACTIVITY_URL } from '../http-utils';
+import { INACTIVE_ACTIVITY_URL, PLACEHOLDER_IMG_URL } from '../http-utils';
+import { useCookies } from 'react-cookie';
 
 export function InactiveActivities(props) {
     const [listActivities, setListActivities] = useState([]);
     const [loading, setLoading] = useState(true);
     const approve = props.func.approve;
+    const [cookies, setCookie, removeCookie] = useCookies(['cookie-name']);
+  
+    const sendJWTConfig = () => {
+        return {
+            headers: {
+                "Authorization": cookies['JWT']
+            }
+        }
+    }
 
     useEffect(() => {
-        axios.get(INACTIVE_ACTIVITY_URL).then(res => {
+        axios.get(INACTIVE_ACTIVITY_URL, sendJWTConfig()).then(res => {
             setListActivities(res.data.map((activity, _) => {
                 return (
                     <div>
@@ -16,7 +26,7 @@ export function InactiveActivities(props) {
                         <h4 className="text-2xl font-bold mb-2">Activity {activity.activity_id}</h4>
                     </div>
                 <div className=" container mx-auto flex flex-wrap items-start my-16">
-                    {/* TODO: <img src={activity.image.path}/> */}
+                    <img src={activity.imagePath ? activity.imagePath : PLACEHOLDER_IMG_URL} alt="activity"/>
                         <p className="text-2xl font-bold mb-2">{activity.info}</p>
                     </div>
                 <div className=" container mx-auto flex flex-wrap items-start my-16">
